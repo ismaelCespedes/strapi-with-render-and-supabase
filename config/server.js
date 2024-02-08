7d2b0aff7d232c0ca26ccdf5ceb85aebee646f20
ismael <flowingsoft.bolivia@gmail.com>
2024-02-08 08:38:30 -0400

    add APP_KEYS property

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['DgrwtKRgNICiu7bQZD027w==','1oan4+MKkPqXPSJu79zUZQ==','QDZzC/VIADPUVQV84qlbnw==','XF/nsMN2HI06SH/gb2xazw==']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
