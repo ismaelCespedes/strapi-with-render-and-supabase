e6e39c638abc7ac87b70d64fcdc85bc4dbe17b4d
ismael <flowingsoft.bolivia@gmail.com>
2024-02-08 09:09:37 -0400

    add admin properties

--- config/admin.js
+++ config/admin.js
@@ -1,17 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', crypto.randomBytes(16).toString('base64')),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', crypto.randomBytes(16).toString('base64')),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', crypto.randomBytes(16).toString('base64')),
     },
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
